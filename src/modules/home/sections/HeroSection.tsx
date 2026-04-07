8a4a7fe3d49d84e8193d4155809d159ac97f59b4
ledung38 <dung.lh@dsc.com.vn>
2026-04-07 11:30:07 +0700

    fix CI

--- src/modules/home/sections/HeroSection.tsx
+++ src/modules/home/sections/HeroSection.tsx
@@ -36,6 +36,7 @@ const HeroSection = () => {
     <section className="relative min-h-screen w-full flex items-end justify-center overflow-hidden pb-0">
       <div className="sm:hidden absolute inset-0 top-20  bg-primary/20  -z-8" />
       <div className="absolute inset-0 -z-10 brightness-50 scale-120">
+        {/* iframe demo process clean car service */}
         <iframe
           className="w-full h-full"
           src="https://www.youtube.com/embed/iM_xmlP0cLg?autoplay=1&mute=1&loop=1&playlist=iM_xmlP0cLg&controls=0&rel=0&modestbranding=1&showinfo=0&fs=0&iv_load_policy=3&color=white&playsinline=1"
